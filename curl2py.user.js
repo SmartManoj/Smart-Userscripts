a4f73caf233cd3ea281001c96f8845ef4639b4a9
SmartManoj <smartmanoj42857@gmail.com>
2019-05-24 20:13:10 +0530

    'Cool'

---
+++ curl2py.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        curl2py
+// @version     0.1
+// @description try to take over the world!
+// @author      SmartManoj
+// @namespace   https://github.com/SmartManoj
+// @match       https://curl.trillworks.com/#python
+// @updateURL   https://raw.githubusercontent.com/SmartManoj/master/curl2py.user.js
+// @downloadURL https://raw.githubusercontent.com/SmartManoj/master/curl2py.user.js
+// ==/UserScript==
+
+
+alert(2)
---
+++ tm.py
@@ -0,0 +1,49 @@
+from time import *	
+import pathlib
+from pyautogui import *
+from glob import *
+from pyperclip import *
+import re
+def local():
+	return f'''// ==UserScript==
+// @name        {name}
+// @version     0.1
+// @description try to take over the world!
+// @author      SmartManoj
+// @namespace   https://github.com/SmartManoj
+// @match       {link}
+// @updateURL   https://raw.githubusercontent.com/SmartManoj/master/{fn}
+// @downloadURL https://raw.githubusercontent.com/SmartManoj/master/{fn}
+// ==/UserScript==
+
+'''
+def browser():
+	return f'''// ==UserScript==
+// @name        {name}
+// @version     0.1
+// @description try to take over the world!
+// @author      SmartManoj
+// @namespace   https://github.com/SmartManoj
+// @match       {link}
+// @require   	{local_path}/{fn}
+// ==/UserScript==
+
+
+'''
+hotkey('win','3') # chrome index
+hotkey('ctrl','shift','h')
+fn=prompt('Enter File name')
+name=prompt('Enter Script name',default=fn)
+sleep(1)
+hotkey('ctrl','a')
+hotkey('ctrl','x')
+local_path=pathlib.Path(__file__).parents[0].as_uri()	
+ext='.user.js'
+l=len(glob(fn+ext))
+if l:fn+=f'_{l+1}'
+fn+=ext
+a=paste()
+link=re.search('@match\s*(.*)',a)[1].strip()
+print(local(),file=open(fn,'w'))
+copy(browser())
+hotkey('ctrl','v')
