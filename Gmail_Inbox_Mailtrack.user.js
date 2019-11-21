58f79e6091ac1d5bcf83d976cdb95b5402c4c1d9
SmartManoj <smartmanoj42857@gmail.com>
2019-11-21 13:06:06 +0530

    'Cool'

--- Gmail_Inbox_Mailtrack.user.js
+++ Gmail_Inbox_Mailtrack.user.js
@@ -8,25 +8,59 @@
 // @updateURL   https://raw.githubusercontent.com/SmartManoj/SmartUserScripts/master/Gmail_Inbox_Mailtrack.user.js
 // ==/UserScript==
 
-
-
-// Usage
-//c  - To open compose box
-//Shift + ? - To turn on keyboard shortcuts for Gmail
-
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-document.onkeydown=async function(e) {
-    'use strict';
-if ( e.key=='c'){
-    console.log('MT');
-    while(!document.querySelector('div#mt-signature')){
-    await sleep(1000);
-    }
-    document.querySelector('div#mt-signature').remove();
-}
-
-
+
+
+
+
+// Usage
+
+//c  - To open compose box
+//r  - To reply
+
+//Shift + ? - To turn on keyboard shortcuts for Gmail
+
+
+
+
+
+function sleep(ms) {
+
+  return new Promise(resolve => setTimeout(resolve, ms));
+
+}
+
+document.onkeydown=async function(e) {
+
+    'use strict';
+
+if ( e.key=='c'){
+
+    console.log('MT');
+
+    while(!document.querySelector('div#mt-signature')){
+
+    await sleep(1000);
+
+    }
+
+    document.querySelector('div#mt-signature').remove();
+
+}
+
+
+if ( e.key=='r'){
+
+    console.log('MT');
+
+    while(!document.querySelector('div#mt-signature')){
+
+    await sleep(1000);
+
+    }
+
+    document.querySelector('div#mt-signature').remove();
+
+}
+
+
 }
